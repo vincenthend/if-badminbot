b562e20312b3b1f10532de43432af9176dfbfcdb
Vincent Halim <vincenthendrha@gmail.com>
2024-04-15 20:21:41 +0800

    feat: change const

--- src/functions/scan_five_day.ts
+++ src/functions/scan_five_day.ts
@@ -6,13 +6,15 @@ import { sendAlert } from 'src/utils/alert'
 import { TelegramAPI } from 'src/apis/telegram/types'
 import { setMessageIdByEvent } from 'src/utils/store'
 
+const SCAN_RANGE = 5
+
 function sendReminder(event: CalendarEvent) {
   const targetChannels = CHANNEL_IDS.filter((channel) =>
     channel.test ? channel.test(event) : true,
   )
   const messageText = `
 *[🏸 Open Registration!]*
-Hello! Kita bakal ada badmin 5 hari lagi di:
+Hello! Kita bakal ada badmin ${SCAN_RANGE} hari lagi di:
     
 📅 *Tanggal*: ${formatDate(event.getStartTime() as Date)}
 ⏰ *Waktu*: ${formatTime(event.getStartTime() as Date)} - ${formatTime(event.getEndTime() as Date)}
@@ -33,7 +35,7 @@ React di message ini ya kalo mau join!
 
 export function scanEvents5Days() {
   try {
-    const events = getNextNDaysEvents(5, 7)
+    const events = getNextNDaysEvents(SCAN_RANGE)
     if (events.length) {
       for (const event of events) {
         sendReminder(event)
